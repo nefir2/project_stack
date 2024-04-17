c82c8704ebc5a5ae1c5392fca7f609aff08ad5e9
nefir2 <vadim.ahtamov@gmail.com>
2024-04-17 23:55:58 +0300

    clean

--- stack_kateassy/Classes/SimpleStack.cs
+++ stack_kateassy/Classes/SimpleStack.cs
@@ -10,35 +10,30 @@ namespace stack_kateassy.Classes
 	{
 		private List<int> stack;
 		public SimpleStack() => stack = new List<int>();
-		public SimpleStack(int[] stack) : this(stack.ToList<int>()) { } // => this.stack = stack.ToList<int>();
+		public SimpleStack(int[] stack) : this(stack.ToList<int>()) { }
 		public SimpleStack(List<int> stack)
 		{
 			stack ??= new List<int>();
 			this.stack = new List<int>(stack.Count);
 			for (int i = 0; i < stack.Count; i++) this.stack.Add(stack[i]);
 		}
-		public SimpleStack(Stack<int> stack) : this(stack.ToList<int>()) { } // => this.stack = stack.ToList<int>();
-		public SimpleStack(SimpleStack stack) : this(stack.AsList()) { } // => this.stack = new List<int>() { (stack.Clone() as SimpleStack) };
+		public SimpleStack(Stack<int> stack) : this(stack.ToList<int>()) { }
+		public SimpleStack(SimpleStack stack) : this(stack.AsList()) { }
 		public void Clear() => stack.Clear();
 		public bool IsEmpty() => stack.Count == 0;
 		public int Pop()
 		{
 			if (this.IsEmpty()) return 0;
-			int value = stack[^1]; //stack.Count - 1 //.Last();
+			int value = stack[^1];
 			stack.RemoveAt(stack.Count - 1);
 			return value;
 		}
 		public void Push(int value) => stack.Add(value);
 		public int Size() => stack.Count;
-		internal void Reverse() => stack.Reverse();
+		private void Reverse() => stack.Reverse();
 		private List<int> AsList() => stack;
 		public object Clone() => new SimpleStack(stack.ToList<int>());
 		internal static SimpleStack Reverse(SimpleStack s) => (CreateStack(s) as SimpleStack) ?? new SimpleStack(s);
-		//{
-		//	s = new SimpleStack((s.Clone() as SimpleStack)?.AsList());
-		//	s.Reverse();
-		//	return s;
-		//}
 		public static IStack CreateStack(Stack<int> s) => new SimpleStack(s.Reverse().ToList<int>());
 		public static IStack CreateStack(IStack s)
 		{
